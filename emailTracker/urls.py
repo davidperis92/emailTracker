fbf3954334aaa1a04b4673aba35b2e690c44434d
David Peris Martinez <david.peris92@gmail.com>
2016-03-15 12:42:27 +0100

    first commit

--- emailTracker/urls.py
+++ emailTracker/urls.py
@@ -8,10 +8,11 @@ app_name = 'emailTracker'
 urlpatterns = [
     url(r'login', views.login),
     url(r"^$", TemplateView.as_view(template_name = app_name+"/index.html")),
-    url(r'^(?P<pk>[0-9]+)/home/', include([
-        url(r'^$', views.HomeView.as_view(), name='home'),
-        url(r'^results/taiga_id/(?P<task_id>)$', views.ResultsView.get_emails_by_taskId),
-        url(r'^results/subject/(?P<subject>)$', views.ResultsView.get_emails_by_subject),
-        url(r'^results/sender/(?P<sender>)$', views.ResultsView.get_emails_by_sender),
+    url(r'^home/$', views.HomeView.as_view(), name='home'),
+    url(r'^results/$', name='results', include([
+        url(r'^taiga_id/(?P<task_id>)$', views.ResultsView.get_emails_by_taskId),
+        url(r'^subject/(?P<subject>)$', views.ResultsView.get_emails_by_subject),
+        url(r'^sender/(?P<sender>)$', views.ResultsView.get_emails_by_sender),
     ])),
+
 ]
